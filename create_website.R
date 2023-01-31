4986b671c0ff712461350f506b37831e9f52ed6e
gerrymcquade <112185858+gerrymcquade@users.noreply.github.com>
2023-01-31 01:49:11 +0000

    basic website with postcard homepage

---
+++ create_website.R
@@ -0,0 +1,28 @@
+
+# install required packages
+install.packages(c("distill", "postcards", "fontawesome"))
+#install.packages(rmarkdown) #should already be installed with rstudio
+
+# load packages
+#library(rmarkdown) #should already be loaded in rstudio
+library(distill) #builds the overall website and blog
+library(postcards) #creates the stylised homepage postcard w/ social links
+library(fontawesome) #used when changing font and theme
+
+#create distill blog website, with folder structure set up for github pages
+distill::create_blog(dir = ".",
+                        title = "Gerald McQuade",
+                        gh_pages = TRUE)
+#NOW CLOSE RSTUDIO THEN REOPEN PROJECT AND BUILD WEBSITE IN BUILD PANE
+#then edit _site.yml
+
+#add postcards page-------------------------------------------------------------
+distill::create_article(file = "postcard",  
+                         template = "trestles",    
+                         package = "postcards")
+
+#now open the postcard.rmd and edit
+
+#|TO MAKE POSTCARD THE HOMEPAGE: paste YAML "site: distill::distill_website" from 
+#|index.rmd into the YAML header of postcard.rmd. then rename index.rmd as blog.rmd
+#|and rename postcard.rmd as index.rmd,
